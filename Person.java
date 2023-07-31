2a0732abfc1818d86578ac6021ab3e0edd2247f6
TrungNguyen1995 <137511421+TrungNguyen1995@users.noreply.github.com>
2023-07-31 15:00:57 +0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,59 @@
+public  class Person {
+    public Person(int id, String name, String address, int age) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    protected int id;
+    protected String name;
+    protected   String address;
+    protected int age;
+
+    public  Person (String name){
+        this.name  = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
